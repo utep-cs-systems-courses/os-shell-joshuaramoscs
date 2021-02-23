5f6fe9bfaf2f932d57cd425f898d1f42307cd2ae
student <student@systems-vm.localdomain>
2021-02-22 19:57:53 -0700

    FD redirect working

--- shell/my_shell.py
+++ shell/my_shell.py
@@ -3,8 +3,12 @@
 import os, sys, time, re
 from my_readLine import my_readLine
 
+if not os.environ["PS1"]:        # If prompt is not set, then set it to '$'
+    os.environ["PS1"] = '$'
+    
+
 while(True):                          # Loop forever
-    os.write(1,"my_shell:$ ".encode()) # Print shell prompt
+    os.write(1,("%s" %os.environ["PS1"]).encode()) # Print shell prompt
     args = my_readLine().split()      # Tokenize input
 
     if args == []:                    # If no input, continue
@@ -20,9 +24,21 @@ while(True):                          # Loop forever
 
     if rc < 0:                                        # Failed forking
         os.write(2, ("fork failed, returning %d\n" % rc).encode())
-        sys.exit(1)                                   # Terminate with error
+        sys.exit(1)                                   # Terminate with error 1
 
     elif rc == 0:                                     # Child
+        if '>' in args:                               # Redirect child's fd output
+            os.close(1)                               # Close display fd and replace it with file
+            os.open(args[args.index('>')+1], os.O_CREAT | os.O_WRONLY);
+            os.set_inheritable(1, True)
+            args.remove(args[args.index('>')+1])
+            args.remove('>')
+        if '<' in args:                               # Redirect child's fd input
+            os.close(0)                               # Close keyboard fd and replace it with file
+            os.open(args[args.index('<')+1], os.O_RDONLY);
+            os.set_inheritable(0, True)
+            args.remove(args[args.index('<') + 1])
+            args.remove('<')
         # os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
         for dir in re.split(":", os.environ['PATH']): # Try each directory in the path
             program = "%s/%s" % (dir, args[0])
@@ -34,7 +50,7 @@ while(True):                          # Loop forever
 
         # os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
         os.write(1, ("\"%s\" is not an available command in your system.\n" % args[0]).encode())
-        sys.exit(1)                                   # terminate with error
+        sys.exit(2)                                   # terminate with error 2
 
     else:                                             # Parent (forked ok)
         # os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
